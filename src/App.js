0f234f087d23b3078089150fe9662f6754f0aaa4
AhmedYasser-gu <gusyasah@student.gu.se>
2022-05-27 14:09:47 +0200

    Components pt2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import './App.css';
+import React, {Component} from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
 
-function App() {
-  return (
+class App extends Component {
+
+    constructor(props){
+      super(props);
+
+      this.state = {
+        dishes:DISHES
+      }
+    }
+
+  render (){
+
+    return (
     <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante ConFusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
     </div>
-  );
+    );
+  }
 }
-
 export default App;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,60 +1,55 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import { Card ,CardImg , CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
+
     constructor(props) {
         super(props);
+
         this.state = {
-            dishes: [
-                {
-                  id: 0,
-                  name:'Uthappizza',
-                  image: 'assets/images/uthappizza.png',
-                  category: 'mains',
-                  label:'Hot',
-                  price:'4.99',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
-               {
-                  id: 1,
-                  name:'Zucchipakoda',
-                  image: 'assets/images/zucchipakoda.png',
-                  category: 'appetizer',
-                  label:'',
-                  price:'1.99',
-                  description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
-               {
-                  id: 2,
-                  name:'Vadonut',
-                  image: 'assets/images/vadonut.png',
-                  category: 'appetizer',
-                  label:'New',
-                  price:'1.99',
-                  description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-               {
-                  id: 3,
-                  name:'ElaiCheese Cake',
-                  image: 'assets/images/elaicheesecake.png',
-                  category: 'dessert',
-                  label:'',
-                  price:'2.99',
-                  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
-               ],
-        };
+            selectedDish: null
+
+        }
+    }
+
+    onDishSelect(dish){
+        this.setState({selectedDish: dish});
+    }
+
+    renderDish(dish){
+        if (dish != null) {
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle><i>{dish.name}</i></CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+
+                </Card>
+            )
+
+
+        }
+        else {
+            return(
+                <div></div>
+            );
+        }
     }
 
     render() {
-        const menu = this.state.dishes.map((dish) => {
+
+        const menu = this.props.dishes.map((dish) => {
+
             return (
-              <div key={dish.id} className="col-12 mt-5">
-                <Media tag="li">
-                  <Media left middle>
-                      <Media object src={dish.image} alt={dish.name} />
-                  </Media>
-                  <Media body className="ml-5">
-                    <Media heading><i>{dish.name}</i></Media>
-                    <p>{dish.description}</p>
-                  </Media>
-                </Media>
+              <div key={dish.id} className="col-12 col-md-5 mt-1">
+                <Card onClick={()=> this.onDishSelect(dish)} >
+                  <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                         <CardTitle><i>{dish.name}</i></CardTitle>
+                    </CardImgOverlay>
+                </Card>
               </div>
             );
         });
@@ -62,9 +57,10 @@ class Menu extends Component {
         return (
           <div className="container">
             <div className="row">
-              <Media list>
                   {menu}
-              </Media>
+            </div>
+            <div className="row">
+                {this.renderDish(this.state.selectedDish)}
             </div>
           </div>
         );
