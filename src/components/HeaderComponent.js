d9781ec412b2abd86648d362505e8c85d0444eb4
AhmedYasser-gu <gusyasah@student.gu.se>
2022-06-01 15:28:05 +0200

    Added Jumbotron bootstrap 5

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 class Header extends Component {
   render() {
-    return(
-    <React.Fragment>
-      <Navbar dark>
-        <div className="container">
+    return (
+      <React.Fragment>
+        <Navbar dark>
+          <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <div class="jumbotron m-3">
+          <div className="container">
+            <div className="row row-header">
+              <div className="col-12 col-sm-6">
+                <h1>Ristorante con Fusion</h1>
+                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+              </div>
+            </div>
+          </div>
         </div>
-      </Navbar>
-      
-           <div className="container">
-               <div className="row row-header">
-                   <div className="col-12 col-sm-6">
-                       <h1>Ristorante con Fusion</h1>
-                       <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
-                   </div>
-               </div>
-           </div>
-
-    </React.Fragment>
+      </React.Fragment>
     );
   }
 }
