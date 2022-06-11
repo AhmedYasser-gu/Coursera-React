810e1ba6035ea25067dd7a570526632472a8149a
AhmedYasser-gu <gusyasah@student.gu.se>
2022-06-11 11:18:15 +0200

    A3 done

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,16 +1,174 @@
-import React from "react";
-import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem
-} from 'reactstrap';
+/* eslint-disable react/jsx-pascal-case */
+import React, { Component } from "react";
+import { Card, CardImg, CardTitle, Breadcrumb, BreadcrumbItem, CardBody, CardText } from "reactstrap";
 import { Link } from 'react-router-dom';
+import {
+    Button, Modal, ModalBody, ModalHeader, Label, Row, Col
+} from "reactstrap";
 
-// Assignment 1 Tasks - Coursera
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
 
-// Task 1 & 2 Render dish details when selected 
+
+const required = (val) => val && val.length; 
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => (val) && (val.length >= len);
+
+class CommentForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+            isCommentFormModalOpen: false
+        };
+
+        this.toggleCommentFormModal = this.toggleCommentFormModal.bind(this);
+        this.handleCommentFormSubmit = this.handleCommentFormSubmit.bind(this);
+
+    }
+
+    handleCommentFormSubmit(values) {
+        console.log("Current State is: " + JSON.stringify(values));
+        alert("Current State is: " + JSON.stringify(values));
+
+
+    }
+
+    toggleCommentFormModal() {
+        this.setState({
+            isCommentFormModalOpen: !this.state.isCommentFormModalOpen
+        });
+    }
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <Button outline onClick={this.toggleCommentFormModal}>
+                    <span className="fa fa-comments fa-lg"></span> Submit Comment
+                </Button>
+
+
+                {/* commentform  Modal */}
+                <Modal isOpen={this.state.isCommentFormModalOpen} toggle={this.toggleCommentFormModal} >
+                    <ModalHeader toggle={this.toggleCommentFormModal}> Submit Comment </ModalHeader>
+                    <ModalBody>
+
+                        <LocalForm onSubmit={(values) => this.handleCommentFormSubmit(values)}>
+
+                            {/* rating */}
+                            <Row className="form-group">
+                                <Label htmlFor="rating" md={12} >Rating</Label>
+                                <Col md={12}>
+                                    <Control.select model=".rating"
+                                        className="form-control"
+                                        name="rating"
+                                        id="rating"
+                                        validators={{
+                                            required
+                                        }}
+                                    >
+                                        <option>Please Select</option>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+
+
+                            {/* author */}
+                            <Row className="form-group">
+                                <Label htmlFor="author" md={12}> Your Name </Label>
+                                <Col md={12}>
+                                    <Control.text model=".author" id="author" name="author"
+                                        placeholder="First Name"
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+
+
+
+
+                            {/* comment */}
+                            <Row className="form-group">
+                                <Label htmlFor="comment" md={12}>Comment</Label>
+                                <Col md={12}>
+                                    <Control.textarea model=".comment" id="comment" name="comment"
+                                        rows="6"
+                                        className="form-control"
+                                        validators={{
+                                            required
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                        }}
+                                    />
+                                </Col>
+
+                            </Row>
+
+                            {/* submit button */}
+                            <Row className="form-group">
+                                <Col>
+                                    <Button type="submit" color="primary">
+                                        Submit
+                                    </Button>
+                                </Col>
+                            </Row>
+
+                        </LocalForm>
+
+                    </ModalBody>
+                </Modal>
+
+
+            </React.Fragment>
+        );
+    }
+}
+
+/**........................ comment component ends ................................................. */
+
+
+
+
+
+
+
+
 function RenderDish({ dish }) {
-
     if (dish != null) {
         return (
             <div className='col-12 col-md-5 m-1'>
@@ -24,53 +182,43 @@ function RenderDish({ dish }) {
             </div>
         );
     }
-
     else {
         return (
-            <div>
-
-            </div>
+            <div></div>
         );
     }
 }
 
-// Task 3 showing Comments
-function RenderComments({ comments }) {
-
-    if (comments != null) {
-        const theComment = comments.map(comment => {
-            return (
-                <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p> -- {comment.author} &nbsp; , &nbsp;
-                        {new Intl.DateTimeFormat('en-US', {
-                            year: 'numeric',
-                            month: 'long',
-                            day: '2-digit'
-                        }).format(new Date(comment.date))}
-                    </p>
-                </li>
-            )
-        })
+function RenderComments({ dish, comments }) {
+    if (comments == null) {
+        return (<div></div>)
+    }
+    const cmnts = comments.map(comment => {
         return (
-            <div className='col-12 col-md-5 m-1'>
-                <h4> Comments </h4>
 
-                <ul className='list-unstyled'>
-                    {theComment}
-                </ul>
+            <li key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>-- {comment.author},
+                    &nbsp;
+                    {new Intl.DateTimeFormat('en-US', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: '2-digit'
+                    }).format(new Date(comment.date))}
+                </p>
+            </li>
 
-            </div>
         )
-    }
-
-    else {
-        return (
-            <div>
-
-            </div>
-        );
-    }
+    })
+    return (
+        <div className='col-12 col-md-5 m-1'>
+            <h4> Comments </h4>
+            <ul className='list-unstyled'>
+                {cmnts}
+            </ul>
+            <CommentForm dish={dish} comments={comments} />
+        </div>
+    )
 }
 
 
@@ -78,39 +226,39 @@ const DishDetail = (props) => {
 
     const dish = props.dish
 
-    // For debugging 
-    console.log(dish);
 
     if (dish == null) {
-        return (
-            <div>
-
-            </div>
-        );
+        return (<div></div>);
     }
+
     return (
         <div className="container">
             <div className="row">
                 <Breadcrumb>
-
-                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    <BreadcrumbItem>
+                        <Link to="/menu">Menu</Link>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>
+                        {props.dish.name}
+                    </BreadcrumbItem>
                 </Breadcrumb>
+
                 <div className="col-12">
-                    <h3>{props.dish.name}</h3>
+                    <h3> {props.dish.menu}</h3>
                     <hr />
                 </div>
             </div>
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <RenderDish dish={props.dish} />
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    <RenderComments comments={props.comments} />
-                </div>
+
+            <div className='row'>
+                <RenderDish dish={props.dish} />
+                <RenderComments dish={props.dish} comments={props.comments} />
             </div>
+
+
         </div>
-    );
+    )
 }
+
+
 
 export default DishDetail;
