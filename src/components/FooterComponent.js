2fce4f2140a46bfc54333d1c20e8811e999c4a69
Ahmed Yasser <70852038+AhmedYasser-gu@users.noreply.github.com>
2022-06-12 12:13:32 +0200

    Update FooterComponent.js
    css fix

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -6,7 +6,7 @@ function Footer(props) {
         <div className="footer">
             <div className="container">
                 <div className="row justify-content-center">
-                    <div className="col-4 offset-1 col-sm-2">
+                    <div className="col-3 offset-2 col-sm-5">
                         <h5>Links</h5>
                         <ul className="list-unstyled">
                             <li><Link to='/home'>Home</Link></li>
@@ -15,7 +15,7 @@ function Footer(props) {
                             <li><Link to='/contactus'>Contact Us</Link></li>
                         </ul>
                     </div>
-                    <div className="col-7 col-sm-5">
+                    <div className="col-5 col-sm-4">
                         <h5>Our Address</h5>
                         <address>
                             121, Clear Water Bay Road<br />
@@ -27,7 +27,7 @@ function Footer(props) {
                                 confusion@food.net</a>
                         </address>
                     </div>
-                    <div className="col-12 col-sm-4 align-self-center">
+                    <div className="col-11 col-sm-3 align-self-center">
                         <div className="text-center">
                             <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-google-plus"></i></a>
                             <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook"></i></a>
