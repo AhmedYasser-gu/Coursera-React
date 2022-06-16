5ae398a70e9924bc218db9154eb6a9fa48a882f5
Ahmed Yasser <70852038+AhmedYasser-gu@users.noreply.github.com>
2022-06-16 13:26:04 +0200

    Update HeaderComponent.js

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -56,7 +56,7 @@ class Header extends Component{
                         <NavbarToggler onClick={this.toggleNav} />
 
                         <NavbarBrand className="mr-auto" href="/">
-                            <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
+                            <img src="assets/images/logo.png" height="31" width="42" alt="Ristorante Con Fusion" />
                         </NavbarBrand>
 
                         <Collapse isOpen={this.state.isNavOpen} navbar>
